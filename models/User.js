0911eb1a89b7c4a5767e0adb451691a72b4cc31f
YonB <yon_baermund@hotmail.com>
2023-03-09 12:04:29 -0500

    got the package file ready, worked on the models following the instructions. workin on the server and connection next.

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model, Types} = require("mongoose");
+
+
+const userSchema = new Schema(
+    {
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+        email: {
+        type: String,
+        required: "Email address is required",
+        match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+        ],
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+        },
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        },
+    ],
+    },
+    {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+    }
+);
+
+userSchema.virtual("friendsCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
